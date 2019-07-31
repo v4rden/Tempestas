8f6bfafd7c3411516a17b19235d3223c33292e3d
v4rden <denys.variah@gmail.com>
2019-07-31 16:45:09 +0300

    Basic implementation of apixu client.

---
+++ Tempestas.Infrastructure/ApixuWeatherClient.cs
@@ -0,0 +1,34 @@
+namespace Tempestas.Infrastructure
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Application.Interfaces;
+    using Application.Weather;
+    using Newtonsoft.Json;
+
+    public class ApixuWeatherClient : BaseWeatherClient
+    {
+        public ApixuWeatherClient()
+        {
+            ApiUrl = "http://api.apixu.com/v1/current.json?key=041be6d421854a20962234134193007&q=";
+            ApiKey = "";
+        }
+        protected override async Task<CurrentWeatherInTownModel> Convert(string json)
+        {
+            dynamic smth = JsonConvert.DeserializeObject(json);
+            string townName = smth.location.name;
+            decimal temperature = smth.current.temp_c;
+            decimal pressure = smth.current.pressure_in;
+            decimal humidity = smth.current.humidity;
+
+            return new CurrentWeatherInTownModel
+            {
+                TownName = townName,
+                Temperature = temperature,
+                Pressure = pressure,
+                Humidity = humidity
+            };
+        }
+    }
+}
