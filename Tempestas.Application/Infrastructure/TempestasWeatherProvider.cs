32f4f03dea21d8b25bd8a76e17905c88ff259811
v4rden <denys.variah@gmail.com>
2019-07-31 16:48:52 +0300

    Basic weather provider implementation.

---
+++ Tempestas.Application/Infrastructure/TempestasWeatherProvider.cs
@@ -0,0 +1,34 @@
+namespace Tempestas.Application.Infrastructure
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Weather;
+
+    public class TempestasWeatherProvider : IWeatherProvider
+    {
+        private readonly List<IWeatherClient> _clients;
+
+        public TempestasWeatherProvider(IWeatherClient todoClient)
+        {
+            _clients = new List<IWeatherClient>();
+            _clients.Add(todoClient);
+        }
+        
+        public async Task<CurrentWeatherInTownModel> GetWeatherAsync(GetWeatherMsg msg,
+            CancellationToken cancellationToken)
+        {
+            
+            var work = new List<Task<CurrentWeatherInTownModel>>();
+            
+            foreach (var weatherClient in _clients)
+            {
+                work.Add( weatherClient.GetWeatherAsync(msg, cancellationToken));
+            }
+
+            var result = await Task.WhenAny(work).Result;
+            return result;
+        }
+    }
+}
