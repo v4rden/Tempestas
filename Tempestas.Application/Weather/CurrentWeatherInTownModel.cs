ea08cecc968afca3a00c79731ae469821d0c245b
v4rden <denys.variah@gmail.com>
2019-07-31 16:45:30 +0300

    Added fields.

--- Tempestas.Application/Weather/CurrentWeatherInTownModel.cs
+++ Tempestas.Application/Weather/CurrentWeatherInTownModel.cs
@@ -3,5 +3,8 @@ namespace Tempestas.Application.Weather
     public class CurrentWeatherInTownModel
     {
         public string TownName { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal Pressure { get; set; }
+        public decimal Humidity { get; set; }
     }
 }
